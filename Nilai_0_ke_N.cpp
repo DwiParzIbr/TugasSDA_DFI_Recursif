69b8f24578bada9754afa6a5d670614a9fc79b30
DwiParzIbr <99091149+DwiParzIbr@users.noreply.github.com>
2022-06-06 02:04:04 +0700

    Create Nilai_0_ke_N.cpp

---
+++ Nilai_0_ke_N.cpp
@@ -0,0 +1,25 @@
+//Dwifi Parizza Ibrahim
+//G1A021092
+#include<iostream>
+using namespace std;
+int i;
+int angka(int n )
+{
+    cout<<"Menulis angka dari 0-N"<<endl;
+    cout<<"Metode DFI_092"<<endl;
+    cout<<"MASUKKAN ANGKA :";
+    cin>>n;
+     for (i=0; i<n; i++)
+    {
+        cout<<"Rekursi Dari 0 Ke N : "<<i;
+        cout<<endl;
+    }
+    return n;
+}
+
+int main ()
+{
+    int n;
+    int i;
+    cout<<angka(i);
+}
