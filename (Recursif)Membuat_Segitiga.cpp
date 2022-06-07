d30a71753e12430f77186abccd1cf488ea2d2ba0
DwiParzIbr <99091149+DwiParzIbr@users.noreply.github.com>
2022-06-07 10:26:17 +0700

    Create (Recursif)Membuat_Segitiga.cpp

---
+++ (Recursif)Membuat_Segitiga.cpp
@@ -0,0 +1,41 @@
+//Dwifi Parizza Ibrahim
+//G1A021092
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+int i, j, k,n;
+
+   
+
+    cout << "Program Membuat Segitga \n";
+
+    cout << "masukkan tinggi = "; cin >> n; cout<<endl;
+
+    for (i = 1; i <=n; i++)
+
+    {
+
+          for (j = 0; j <(n - i); j++)
+
+                cout <<" ";
+
+          for (j = 1; j <= i; j++)
+
+                cout <<"*";
+
+          for (k = 1; k < i; k++)
+
+                cout <<"*";
+
+          cout <<"\n";
+
+    }
+}
